76c55045371f16eddba862bb145a77b9edc4efac
valera7979 <valera7979@mail.ru>
2017-05-08 14:37:49 +0300

    model. dependencies in pom

---
+++ src/main/java/model/todo.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by Валера on 08.05.2017.
+ */
+public class todo {
+
+    private Integer id;
+
+    private String description;
+
+    private LocalDateTime started;
+
+    private LocalDateTime expiration_date;
+
+    private String status;
+
+}
---
+++ src/main/resources/db/init_db.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS todo;
+
+CREATE SEQUENCE global_seq START 100000;
+
+CREATE TABLE todo
+(
+  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  description       VARCHAR NOT NULL,
+  started TIMESTAMP DEFAULT now(),
+  expiration_date TIMESTAMP,
+  status VARCHAR NOT NULL
+
+);
